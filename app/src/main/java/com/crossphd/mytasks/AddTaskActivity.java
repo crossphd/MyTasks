a6466e22573886553621c1565415926025f7c1b2
Christopher Ross <cross25@uis.edu>
2018-02-08 13:10:47 -0800

    first full commit

---
+++ app/src/main/java/com/crossphd/mytasks/AddTaskActivity.java
@@ -0,0 +1,78 @@
+package com.crossphd.mytasks;
+
+import android.content.ContentValues;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+/**
+ * Created by chris on 2/8/2018.
+ */
+
+public class AddTaskActivity extends AppCompatActivity {
+
+    // Declare a member variable to keep track of a task's selected mPriority
+    private int mPriority;
+    private boolean mCompleted;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+
+        // Initialize to highest mPriority by default (mPriority = 2)
+        ((RadioButton) findViewById(R.id.radButton1)).setChecked(true);
+        mPriority = 2;
+        mCompleted = false;
+    }
+
+
+    /**
+     * onClickAddTask is called when the "ADD" button is clicked.
+     * It retrieves user input and inserts that new task data into the underlying database.
+     */
+    public void onClickAddTask(View view) {
+
+        String input = ((EditText) findViewById(R.id.editTextTaskDescription)).getText().toString();
+        if (input.length() == 0) {
+            return;
+        }
+
+        // Insert new task data via a ContentResolver
+        // Create new empty ContentValues object
+        ContentValues contentValues = new ContentValues();
+        // Put the task description and selected mPriority into the ContentValues
+        contentValues.put(TaskContract.TaskEntry.COLUMN_DESCRIPTION, input);
+        contentValues.put(TaskContract.TaskEntry.COLUMN_PRIORITY, mPriority);
+        contentValues.put(TaskContract.TaskEntry.COLUMN_COMPLETED, mCompleted);
+        // Insert the content values via a ContentResolver
+        Uri uri = getContentResolver().insert(TaskContract.TaskEntry.CONTENT_URI, contentValues);
+
+
+//        if(uri != null) {
+//            Toast.makeText(getBaseContext(), uri.toString(), Toast.LENGTH_LONG).show();
+//        }
+
+        // Finish activity (this returns back to MainActivity)
+        finish();
+
+    }
+
+
+    /**
+     * onPrioritySelected is called whenever a priority button is clicked.
+     * It changes the value of mPriority based on the selected button.
+     */
+    public void onPrioritySelected(View view) {
+        if (((RadioButton) findViewById(R.id.radButton1)).isChecked()) {
+            mPriority = 1;
+        } else if (((RadioButton) findViewById(R.id.radButton2)).isChecked()) {
+            mPriority = 2;
+        } else if (((RadioButton) findViewById(R.id.radButton3)).isChecked()) {
+            mPriority = 3;
+        }
+    }
+}
---
+++ app/src/main/java/com/crossphd/mytasks/TasksActivity.java
@@ -0,0 +1,199 @@
+package com.crossphd.mytasks;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
+public class TasksActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    // Constants for logging and referring to a unique loader
+    private static final String TAG = TasksActivity.class.getSimpleName();
+    private static final int TASK_LOADER_ID = 0;
+
+    private CursorAdapter mAdapter;
+    private CheckBox mCheckBox;
+
+
+    RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tasks);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerViewTasks);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter = new CursorAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
+        mCheckBox = (CheckBox) findViewById(R.id.completed);
+
+         /*
+         Add a touch helper to the RecyclerView to recognize when a user swipes to delete an item.
+         An ItemTouchHelper enables touch behavior (like swipe and move) on each ViewHolder,
+         and uses callbacks to signal when a user is performing these actions.
+         */
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            // Called when a user swipes left or right on a ViewHolder
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                // Here is where you'll implement swipe to delete
+                int id = (int) viewHolder.itemView.getTag();
+                String idString = Integer.toString(id);
+
+                Uri uri = (TaskContract.TaskEntry.CONTENT_URI).buildUpon().appendPath(idString).build();
+
+                getContentResolver().delete(uri, null, null);
+
+                getSupportLoaderManager().restartLoader(TASK_LOADER_ID, null, TasksActivity.this);
+
+            }
+        }).attachToRecyclerView(mRecyclerView);
+
+        /*
+         Set the Floating Action Button (FAB) to its corresponding View.
+         Attach an OnClickListener to it, so that when it's clicked, a new intent will be created
+         to launch the AddTaskActivity.
+         */
+        FloatingActionButton fabButton = (FloatingActionButton) findViewById(R.id.fab);
+
+        fabButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to start an AddTaskActivity
+                Intent addTaskIntent = new Intent(TasksActivity.this, AddTaskActivity.class);
+                startActivity(addTaskIntent);
+            }
+        });
+
+        /*
+         Ensure a loader is initialized and active. If the loader doesn't already exist, one is
+         created, otherwise the last created loader is re-used.
+         */
+        getSupportLoaderManager().initLoader(TASK_LOADER_ID, null, this);
+
+    }
+
+    /**
+     * This method is called after this activity has been paused or restarted.
+     * Often, this is after new data has been inserted through an AddTaskActivity,
+     * so this restarts the loader to re-query the underlying data for any changes.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // re-queries for all tasks
+        getSupportLoaderManager().restartLoader(TASK_LOADER_ID, null, this);
+    }
+
+    /**
+     * Instantiates and returns a new AsyncTaskLoader with the given ID.
+     * This loader will return task data as a Cursor or null if an error occurs.
+     * <p>
+     * Implements the required callbacks to take care of loading data at all stages of loading.
+     */
+    @SuppressLint("StaticFieldLeak")
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, final Bundle loaderArgs) {
+
+        return new AsyncTaskLoader<Cursor>(this) {
+
+            // Initialize a Cursor, this will hold all the task data
+            Cursor mTaskData = null;
+
+            // onStartLoading() is called when a loader first starts loading data
+            @Override
+            protected void onStartLoading() {
+                if (mTaskData != null) {
+                    // Delivers any previously loaded data immediately
+                    deliverResult(mTaskData);
+                } else {
+                    // Force a new load
+                    forceLoad();
+                }
+            }
+
+            // loadInBackground() performs asynchronous loading of data
+            @Override
+            public Cursor loadInBackground() {
+                // Will implement to load data
+
+                // Query and load all task data in the background; sort by priority
+                // [Hint] use a try/catch block to catch any errors in loading data
+
+                try {
+                    return getContentResolver().query(TaskContract.TaskEntry.CONTENT_URI,
+                            null,
+                            null,
+                            null,
+                            TaskContract.TaskEntry.COLUMN_PRIORITY);
+
+                } catch (Exception e) {
+                    Log.e(TAG, "Failed to asynchronously load data.");
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+            // deliverResult sends the result of the load, a Cursor, to the registered listener
+            public void deliverResult(Cursor data) {
+                mTaskData = data;
+                super.deliverResult(data);
+            }
+        };
+    }
+
+    /**
+     * Called when a previously created loader has finished its load.
+     *
+     * @param loader The Loader that has finished.
+     * @param data   The data generated by the Loader.
+     */
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // Update the data that the adapter uses to create ViewHolders
+        mAdapter.swapCursor(data);
+    }
+
+    /**
+     * Called when a previously created loader is being reset, and thus
+     * making its data unavailable.
+     * onLoaderReset removes any references this activity had to the loader's data.
+     *
+     * @param loader The Loader that is being reset.
+     */
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mAdapter.swapCursor(null);
+    }
+
+    public void checkboxClicked(View view) {
+        CheckBox checkBox = (CheckBox)view;
+        if(checkBox.isChecked()){
+            Toast.makeText(this, "checkbox clicked on", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this, "checkbox clicked off", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
