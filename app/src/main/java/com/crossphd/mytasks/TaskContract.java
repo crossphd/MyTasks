c34d35c8c37836e1f7422724c480ef3914ecfa35
Christopher Ross <cross25@uis.edu>
2018-02-15 12:19:35 -0800

    more cleanup of code

--- app/src/main/java/com/crossphd/mytasks/TaskContract.java
+++ app/src/main/java/com/crossphd/mytasks/TaskContract.java
@@ -7,34 +7,18 @@ import android.provider.BaseColumns;
  * Created by chris on 2/8/2018.
  */
 
-public class TaskContract {
+class TaskContract {
 
-    // The authority, which is how your code knows which Content Provider to access
-    public static final String AUTHORITY = "com.crossphd.mytasks";
+    static final String AUTHORITY = "com.crossphd.mytasks";
+    static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+    static final String PATH_TASKS = "tasks";
+    static final class TaskEntry implements BaseColumns {
 
-    // The base content URI = "content://" + <authority>
-    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
-
-    // Define the possible paths for accessing data in this contract
-    // This is the path for the "tasks" directory
-    public static final String PATH_TASKS = "tasks";
-
-    /* TaskEntry is an inner class that defines the contents of the task table */
-    public static final class TaskEntry implements BaseColumns {
-
-        // TaskEntry content URI = base content URI + path
-        public static final Uri CONTENT_URI =
-                BASE_CONTENT_URI.buildUpon().appendPath(PATH_TASKS).build();
-
-
-        // Task table and column names
-        public static final String TABLE_NAME = "tasks";
-
-        // Since TaskEntry implements the interface "BaseColumns", it has an automatically produced
-        // "_ID" column in addition to the two below
-        public static final String COLUMN_DESCRIPTION = "description";
-        public static final String COLUMN_PRIORITY = "priority";
-        public static final String COLUMN_COMPLETED = "completed";
+        static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_TASKS).build();
+        static final String TABLE_NAME = "tasks";
+        static final String COLUMN_DESCRIPTION = "description";
+        static final String COLUMN_PRIORITY = "priority";
+        static final String COLUMN_COMPLETED = "completed";
 
     }
 }
